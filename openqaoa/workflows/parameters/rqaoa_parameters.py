8f0e6485de89aadc48408ed1383bbd51a7a8c773
Eze <ezequiel@entropicalabs.com>
2022-05-06 07:53:20 +0000

    Added a QAOA instance as an input for RQAOA to speficy all properties and fixed docstrings

--- openqaoa/workflows/parameters/rqaoa_parameters.py
+++ openqaoa/workflows/parameters/rqaoa_parameters.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from ctypes import Union
+from typing import Union
 from .parameters import Parameters
 from openqaoa.qaoa_parameters import Hamiltonian
 
