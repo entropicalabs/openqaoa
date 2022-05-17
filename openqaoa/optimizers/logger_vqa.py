0c4b59e612e136555ecb26b60fbddef98febfee7
shahidee <shahidee44@hotmail.com>
2022-05-17 12:12:13 +0000

    Removed Commented Print Statements

--- openqaoa/optimizers/logger_vqa.py
+++ openqaoa/optimizers/logger_vqa.py
@@ -173,7 +173,6 @@ class Logger(object):
         # new layer in the structure if the previous layer was updated and all
         # members of the layers were changed from the update.
         for each_index in range(len(self.best_update_structure)):
-#             print(self.best_update_structure[each_index])
             change_count = 0
             for each_member in self.best_update_structure[each_index]:
                 if each_member in input_dict.keys():
@@ -181,7 +180,6 @@ class Logger(object):
                     old_best = logged_var.best.copy()
                     self._log_best(each_member, input_dict[each_member])
                     new_best = logged_var.best
-#                     print(each_member, old_best, new_best)
                     if type(new_best[0]) == np.ndarray:
                         if not np.array_equal(new_best, old_best):
                             change_count += 1
