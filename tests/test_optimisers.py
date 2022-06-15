263b3478248cd3182810220758d3ce4f2229e772
Vishal Sharma <vishalsharma.ph@gmail.com>
2022-06-15 03:40:32 +0000

    removed duplicate import statement

--- tests/test_optimisers.py
+++ tests/test_optimisers.py
@@ -25,8 +25,6 @@ from openqaoa.optimizers import get_optimizer
 from openqaoa.derivative_functions import derivative
 from openqaoa.optimizers.logger_vqa import Logger
 
-from openqaoa.optimizers.logger_vqa import Logger
-
 """
 Unittest based testing of custom optimizers.
 """
