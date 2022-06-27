296c75b54b85c48535b238663b3c310f34c39850
shahidee <shahidee44@hotmail.com>
2022-06-27 08:12:25 +0000

    Updated Devices unittest

--- tests/test_qpu_devices.py
+++ tests/test_qpu_devices.py
@@ -15,7 +15,7 @@
 import unittest
 import json
 
-from openqaoa.devices import DeviceQiskit
+from openqaoa.devices import DeviceQiskit, DeviceLocal, DeviceAWS, SUPPORTED_LOCAL_SIMULATORS
 
 
 """
@@ -55,16 +55,16 @@ class TestingDeviceQiskit(unittest.TestCase):
             self.GROUP = json_obj['GROUP']
             self.PROJECT = json_obj['PROJECT']
 
-        if self.API_TOKEN == "None":
+        if self.API_TOKEN == "YOUR_API_TOKEN_HERE":
             raise ValueError(
                 "Please provide an appropriate API TOKEN in crendentials.json.")
-        elif self.HUB == "None":
+        elif self.HUB == "IBMQ_HUB":
             raise ValueError(
                 "Please provide an appropriate IBM HUB name in crendentials.json.")
-        elif self.GROUP == "None":
+        elif self.GROUP == "IBMQ_GROUP":
             raise ValueError(
                 "Please provide an appropriate IBMQ GROUP name in crendentials.json.")
-        elif self.PROJECT == "None":
+        elif self.PROJECT == "IBMQ_PROJECT":
             raise ValueError(
                 "Please provide an appropriate IBMQ Project name in crendentials.json.")
 
@@ -119,7 +119,107 @@ class TestingDeviceQiskit(unittest.TestCase):
         self.assertEqual(device_obj.check_connection(), False)
         self.assertEqual(device_obj.provider_connected, True)
         self.assertEqual(device_obj.qpu_connected, False)
+        
 
+class TestingDeviceLocal(unittest.TestCase):
+    
+    def test_supported_device_names(self):
+        
+        for each_device_name in SUPPORTED_LOCAL_SIMULATORS:
+            device_obj = DeviceLocal(each_device_name)
+            
+            self.assertEqual(device_obj.check_connection(), True)
+    
+    def test_unsupported_device_names(self):
+        
+        device_obj = DeviceLocal('unsupported_device')
+        
+        self.assertEqual(device_obj.check_connection(), False)
+
+
+class TestingDeviceAWS(unittest.TestCase):
+    
+    def setUp(self):
+
+        try:
+            opened_f = open('./tests/credentials.json', 'r')
+        except FileNotFoundError:
+            opened_f = open('credentials.json', 'r')
+                
+        with opened_f as f:
+            json_obj = json.load(f)['AWS']
+            self.AWS_ACCESS_KEY_ID = json_obj['AWS_ACCESS_KEY_ID']
+            self.AWS_SECRET_ACCESS_KEY = json_obj['AWS_SECRET_ACCESS_KEY']
+            self.AWS_REGION = json_obj['AWS_REGION']
+            self.S3_BUCKET_NAME = json_obj['S3_BUCKET_NAME']
+
+        if self.AWS_ACCESS_KEY_ID == "YOUR_ACCESS_KEY_ID":
+            raise ValueError(
+                "Please provide an appropriate AWS access key ID in crendentials.json.")
+        elif self.AWS_SECRET_ACCESS_KEY == "YOUR_SECRET_ACCESS_KEY":
+            raise ValueError(
+                "Please provide an appropriate AWS secret access key in crendentials.json.")
+        elif self.AWS_REGION == "AWS_REGION":
+            raise ValueError(
+                "Please provide an appropriate AWS region name in crendentials.json.")
+        elif self.S3_BUCKET_NAME == "YOUR_S3_BUCKET":
+            raise ValueError(
+                "Please provide an appropriate S3 bucket name in crendentials.json.")
+            
+    def test_check_connection_provider_no_backend_wrong_credentials(self):
+        
+        device_obj = DeviceAWS(device_name = '', aws_access_key_id = '', 
+                               aws_secret_access_key = '', aws_region = '', 
+                               s3_bucket_name = '')
+        
+        self.assertEqual(device_obj.check_connection(), False)
+        self.assertEqual(device_obj.provider_connected, False)
+        self.assertEqual(device_obj.qpu_connected, None)
+        
+    def test_check_connection_provider_no_backend_provided_credentials(self):
+
+        device_obj = DeviceAWS(device_name='', 
+                               aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+                               aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY, 
+                               aws_region=self.AWS_REGION,
+                               s3_bucket_name=self.S3_BUCKET_NAME)
+
+        self.assertEqual(device_obj.check_connection(), True)
+        self.assertEqual(device_obj.provider_connected, True)
+        self.assertEqual(device_obj.qpu_connected, None)
+
+    def test_check_connection_provider_right_backend_provided_credentials(self):
+
+        device_obj = DeviceAWS(device_name='', 
+                               aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+                               aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY, 
+                               aws_region=self.AWS_REGION,
+                               s3_bucket_name=self.S3_BUCKET_NAME)
+
+        device_obj.check_connection()
+        valid_qpu_name = device_obj.available_qpus[0]
+
+        device_obj = DeviceAWS(device_name=valid_qpu_name, 
+                               aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+                               aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY, 
+                               aws_region=self.AWS_REGION,
+                               s3_bucket_name=self.S3_BUCKET_NAME)
+
+        self.assertEqual(device_obj.check_connection(), True)
+        self.assertEqual(device_obj.provider_connected, True)
+        self.assertEqual(device_obj.qpu_connected, True)
+
+    def test_check_connection_provider_wrong_backend_provided_credentials(self):
+
+        device_obj = DeviceAWS(device_name='random_invalid_backend', 
+                               aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+                               aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY, 
+                               aws_region=self.AWS_REGION,
+                               s3_bucket_name=self.S3_BUCKET_NAME)
+
+        self.assertEqual(device_obj.check_connection(), False)
+        self.assertEqual(device_obj.provider_connected, True)
+        self.assertEqual(device_obj.qpu_connected, False)
 
 if __name__ == '__main__':
     unittest.main()
