e5fa7afa9508fca016d12d06b2bed253c3ef194b
Alejandro Montanez <alejomonbar@gmail.com>
2022-07-19 08:13:05 +0200

    adding docplex to setup

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ requirements = [
     "nbsphinx==0.8.9",
     "ipython==8.2.0" ,
     "nbconvert==6.5.0",
-    "pytest-cov==3.0.0"
+    "pytest-cov==3.0.0",
+    "docplex==2.23.222"
     ]
 
 setup(
