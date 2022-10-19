b206b0434661eb4f7d2d8d3a6e2ef11c5c6d8519
Leonardo Disilvestro <leonardo@entropicalabs.com>
2022-10-19 15:51:07 +0800

    fix qpu notebooks

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,6 +4,7 @@ to a python script via nbconvert and then running the resulting .py file.
 """
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
+import pytest
 
 import sys, os
 import subprocess
@@ -17,7 +18,7 @@ def notebook_test_function(name):
     with open(name, encoding='utf-8') as f:
         nb = nbformat.read(f, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=600, kernel_name='env')
+    ep = ExecutePreprocessor(timeout=600, kernel_name='2022_10_19_oq_dev_main')
 
     ep.preprocess(nb)
 
