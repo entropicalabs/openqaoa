8b05a0f9fa02637c2d9d673d2c637fdfa080c1dc
Leonardo Disilvestro <leonardo@entropicalabs.com>
2022-10-19 14:14:14 +0800

    Update docs and tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -23,38 +23,41 @@ def notebook_test_function(name):
 
 
 # @pytest.mark.notebook
-def test_1_workflows_example():
-    notebook_test_function("./examples/1_workflows_example.ipynb")
+def test_01_workflows_example():
+    notebook_test_function("./examples/01_workflows_example.ipynb")
 
 # @pytest.mark.notebook
-def test_2_simulators_comparison():
-    notebook_test_function("./examples/2_simulators_comparison.ipynb")
+def test_02_simulators_comparison():
+    notebook_test_function("./examples/02_simulators_comparison.ipynb")
+
+@pytest.mark.qpu
+def test_03_qaoa_on_qpus():
+    notebook_test_function("./examples/03_qaoa_on_qpus.ipynb")
 
 # @pytest.mark.notebook
-# def test_3_qaoa_on_qpus():
-#     notebook_test_function("./examples/3_qaoa_on_qpus.ipynb")
+def test_04_qaoa_variational_parameters():
+    notebook_test_function("./examples/04_qaoa_variational_parameters.ipynb")
 
 # @pytest.mark.notebook
-def test_4_qaoa_variational_parameters():
-    notebook_test_function("./examples/4_qaoa_variational_parameters.ipynb")
+def test_05_advanced_parameterization():
+    notebook_test_function("./examples/05_advanced_parameterization.ipynb")
 
 # @pytest.mark.notebook
-def test_5_advanced_parameterization():
-    notebook_test_function("./examples/5_advanced_parameterization.ipynb")
-
-
-# @pytest.mark.notebook
-def test_6_fast_qaoa_simulator():
-    notebook_test_function("./examples/6_fast_qaoa_simulator.ipynb")
+def test_06_fast_qaoa_simulator():
+    notebook_test_function("./examples/06_fast_qaoa_simulator.ipynb")
 
 # @pytest.mark.notebook
-def test_7_cost_landscapes_w_manual_mode():
-    notebook_test_function("./examples/7_cost_landscapes_w_manual_mode.ipynb")
+def test_07_cost_landscapes_w_manual_mode():
+    notebook_test_function("./examples/07_cost_landscapes_w_manual_mode.ipynb")
 
 # @pytest.mark.notebook
-def test_8_results_example():
-    notebook_test_function("./examples/8_results_example.ipynb")
+def test_08_results_example():
+    notebook_test_function("./examples/08_results_example.ipynb")
 
 # @pytest.mark.notebook
-def test_9_RQAOA_example():
-    notebook_test_function("./examples/9_RQAOA_example.ipynb")
+def test_09_RQAOA_example():
+    notebook_test_function("./examples/09_RQAOA_example.ipynb")
+
+@pytest.mark.qpu
+def test_10_workflows_on_Amazon_braket():
+    notebook_test_function("./examples/10_workflows_on_Amazon_braket.ipynb")
