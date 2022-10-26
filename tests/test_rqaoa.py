58042f3f2309eece1ee89715084239835d4a9f37
raulconchello <raulconchello@gmail.com>
2022-10-26 15:14:00 +0800

    Added rqaoa functions tests

--- tests/test_rqaoa.py
+++ tests/test_rqaoa.py
@@ -209,6 +209,82 @@ class TestingRQAOA(unittest.TestCase):
         assert correct_full_solution == full_solution, f'Solution was not computed correctly'
 
 
+    def test_problem_from_dict(self): 
+        """
+        Test the function that computes a calssical Hamiltonian from a given graph, accounting for approriate
+        labelling of the nodes and edges.
+        
+        The test consists in generating the correct QUBO problem for a given graph dictionary.
+        """
+        
+        # Trial graph
+        input_dict = dict({():10,(1,):1,(2,):-1,(6,):4,(1,2):1,(2,5):2,(10,14):3,(6,9):4,(6,14):5,(5,6):6})
+        
+        # Correct hamiltonian
+        correct_dict = dict({(0,):1,(1,):-1,(3,):4,(0,1):1,(1,2):2,(2,3):6,(3,4):4,(3,6):5,(5,6):3})
+        hamiltonian = Hamiltonian.classical_hamiltonian(list(correct_dict.keys()),list(correct_dict.values()), constant = 10)
+        hamiltonian_dict = {term.qubit_indices:coeff for term,coeff in zip(hamiltonian.terms,hamiltonian.coeffs)}
+
+        # Compute hamiltonian from input graph
+        comp_problem = problem_from_dict(input_dict)
+        comp_hamiltonian = comp_problem.hamiltonian()
+        comp_hamiltonian_dict = {term.qubit_indices:coeff for term,coeff in zip(comp_hamiltonian.terms,comp_hamiltonian.coeffs)}
+
+
+        # Test computed Hamiltonian contains the correct terms
+        assert hamiltonian_dict == comp_hamiltonian_dict, f'Terms and coefficients in the computed Hamiltonian are incorrect'
+
+        # Test computed Hamiltonian contains the correct terms
+        assert np.allclose(hamiltonian.constant,comp_hamiltonian.constant), f'Constant in the computed Hamiltonian is incorrect'
+
+    def test_redefine_problem(self): 
+        """
+        Test the function that computes the new QUBO for a reduced problem, given the original
+        QUBO encoding the problem and a set of elimination rules via the spin_map.
+
+        The test consists in computing the new Hamiltonian from a given one and a set of elimination rules.
+        """
+
+        ## Problem definition
+
+        # Number of qubits
+        n_qubits = 10
+
+        # Edges and weights of the graph
+        input_edges = [(i,i+1) for i in range(n_qubits-1)] + [(0,n_qubits-1)]
+        input_weights = [1 for _ in range(len(input_edges))]
+
+        # Input problem
+        input_problem = QUBO(n_qubits, input_edges, input_weights)
+
+        # Input spin map (elimination rules)
+        spin_map = dict({(0,(1,0)),(1,(-1,0)),(2,(1,2)),(3,(1,3)),(4,(1,2)),\
+                (5,(1,2)),(6,(1,6)),(7,(1,7)),(8,(1,8)),(9,(1,0))})
+
+        # Compute new problem
+        comp_problem, _ = redefine_problem(input_problem, spin_map)
+        
+        # Compute the new hamiltonian
+        comp_hamiltonian = comp_problem.hamiltonian()
+
+        ## Testing (Comparing the new hamiltonian with the correct one)
+
+        # Correct edges, weights and hamiltonian for the reduced problem
+        edges = [(0,1),(1,2),(1,3),(3,4),(4,5),(0,5)]
+        weights = [-1,2,1,1,1,1]
+
+        hamiltonian = Hamiltonian.classical_hamiltonian(edges, weights, constant = 0)
+
+        # Test computed Hamiltonian contains the correct terms
+        assert hamiltonian.terms == comp_hamiltonian.terms, f'Terms in the computed Hamiltonian are incorrect'
+
+        # Test computed Hamiltonian contains the correct terms
+        assert np.allclose(hamiltonian.coeffs,comp_hamiltonian.coeffs), f'Coefficients in the computed Hamiltonian are incorrect'
+
+        # Test computed Hamiltonian contains the correct terms
+        assert np.allclose(hamiltonian.constant,comp_hamiltonian.constant), f'Constant in the computed Hamiltonian is incorrect'
+
+
 if __name__ == "__main__":
 	unittest.main()
  
