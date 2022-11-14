203b2248027c53a35b05d717fbf1579d4378bed2
shahidee44@hotmail.com <shahidee44@hotmail.com>
2022-11-14 02:12:32 +0000

    Updated DevicAWS

--- openqaoa/devices.py
+++ openqaoa/devices.py
@@ -286,7 +286,9 @@ class DeviceAWS(DeviceBase):
 		as input to the device_name parameter.
     """
     
-    def __init__(self, device_name: str, folder_name: str = 'openqaoa'):
+    def __init__(self, device_name: str, s3_bucket_name: str = None, 
+                 aws_region: str = None, 
+                 folder_name: str = 'openqaoa'):
         
         """Input the device arn and the name of the folder in which all the
         results for the QPU runs would be saved on the pre-defined s3 bucket. 
@@ -297,6 +299,11 @@ class DeviceAWS(DeviceBase):
         ----------
 		device_name: `str`
 			The ARN string of the braket QPU/simulator to be used
+        s3_bucket_name: `str`
+            The name of S3 Bucket where the Braket run results will be saved.
+        aws_region: `str`
+            The aws region in which the QPU/simulator is located. Defaults to the
+            region set in aws cli config.
         folder_name: `str`
             The name of the folder in the s3 bucket that will contain the results
             from the tasks performed in this run.
@@ -304,6 +311,8 @@ class DeviceAWS(DeviceBase):
         
         self.device_name = device_name
         self.device_location = 'aws'
+        self.s3_bucket_name = s3_bucket_name
+        self.aws_region = aws_region
         self.folder_name = folder_name
         
         self.provider_connected = None
@@ -352,9 +361,8 @@ class DeviceAWS(DeviceBase):
     def _check_provider_connection(self) -> bool:
         
         try:
-            sess = Session()
-            self.aws_session = AwsSession(sess)
-            self.s3_bucket_name = self.aws_session.default_bucket()
+            sess = Session(region_name = self.aws_region)
+            self.aws_session = AwsSession(sess, default_bucket=self.s3_bucket_name)
             return True
         except NoRegionError:
             self.aws_session = None
@@ -378,7 +386,9 @@ def device_class_arg_mapper(device_class:DeviceBase,
                             endpoint_id: str = None,
                             engagement_manager: EngagementManager = None,
                             device_name: str = None,
-                            folder_name: str = None) -> dict:
+                            folder_name: str = None, 
+                            s3_bucket_name:str = None, 
+                            aws_region: str = None) -> dict:
     DEVICE_ARGS_MAPPER = {
         DeviceQiskit: {'api_token': api_token,
                         'hub': hub,
@@ -394,6 +404,8 @@ def device_class_arg_mapper(device_class:DeviceBase,
                         'engagement_manager': engagement_manager},
         
         DeviceAWS: {'device_name': device_name,
+                    's3_bucket_name': s3_bucket_name,
+                    'aws_region': aws_region,
                     'folder_name': folder_name}
     }
 
