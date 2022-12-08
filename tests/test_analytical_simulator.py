39f862677708a219fb5063540095ebc2c55ab876
Kristina Kirova <kidiki97@gmail.com>
2022-12-08 06:38:29 +0000

    end to end tests for RQAOA revealed that I have to pass prepend state, append_state etc in the init so that's changed and the tests run now, though maybe not the most bullet-proofed ones.

--- tests/test_analytical_simulator.py
+++ tests/test_analytical_simulator.py
@@ -21,7 +21,7 @@ from openqaoa.backends.simulators.qaoa_analytical_sim import QAOABackendAnalytic
 from openqaoa.workflows.optimizer import QAOA, RQAOA
 from openqaoa.problems.problem import QUBO, MaximumCut
 from openqaoa.devices import create_device
-from openqaoa.utilities import X_mixer_hamiltonian, ring_of_disagrees
+from openqaoa.utilities import X_mixer_hamiltonian, ring_of_disagrees, random_k_regular_graph
 from openqaoa.qaoa_parameters.baseparams import QAOACircuitParams
 from openqaoa.qaoa_parameters import QAOAVariationalStandardParams
 
@@ -36,14 +36,11 @@ def Disagrees_SetUp(n_qubits, p, mixer_hamil):
     register = range(n_qubits)
     cost_hamil = ring_of_disagrees(register)
     
-
     betas = [np.pi/8]
     gammas = [np.pi/4]
 
     qaoa_circuit_params = QAOACircuitParams(cost_hamil, mixer_hamil, p)
-    variational_params_std = QAOAVariationalStandardParams(qaoa_circuit_params,
-                                                           betas,
-                                                           gammas)
+    variational_params_std = QAOAVariationalStandardParams(qaoa_circuit_params, betas, gammas)
     # Get the part of the Hamiltonian proportional to the identity
 
     return register, cost_hamil, qaoa_circuit_params, variational_params_std
@@ -102,7 +99,46 @@ class TestingQAOABackendAnalyticalSimulator(unittest.TestCase):
         assert exception, "XY mixer Hamiltonian didn't fail."
         
     def test_end_to_end_rqaoa(self):
-        pass
+        # Create a 3-regular weighted graph
+        g = random_k_regular_graph(degree=3, nodes=range(8), seed=2642, weighted=True, biases=False) 
+
+        # Define te problem and translate it into a binary Qubo.
+        maxcut_prob = MaximumCut(g)
+        maxcut_qubo = maxcut_prob.get_qubo_problem()
+
+        # Define the RQAOA object
+        r = RQAOA()
+
+        # Set parameters for RQAOA, in this case we fix the n_max to 1 (default), the final cutoff value to 3
+        r.set_rqaoa_parameters(steps=1, n_cutoff=3)
+
+        ## Setting up the QAOA properties
+        init_beta = 0.26
+        init_gamma = 0.42
+        r.set_circuit_properties(p=1, init_type='custom', variational_params_dict={"betas":[init_beta], "gammas":[init_gamma]}, mixer_hamiltonian='x')
+
+        # Define the device to be the analytical simulator
+        device = create_device(location='local', name='analytical_simulator')
+        r.set_device(device)
+
+        # Set the classical method used to optimiza over QAOA angles and its properties
+        r.set_classical_optimizer(method="rmsprop", 
+                                  optimizer_options = {"stepsize" : 10**(-10)}, tol=10**(-1), 
+                                  maxfev=1, maxiter=1, 
+                                  jac='finite_difference', 
+                                  optimization_progress=False, cost_progress=False, parameter_log=False)
+
+        # Compile and optimize the problem instance on RQAOA
+        r.compile(maxcut_qubo)
+        r.optimize()
+
+        opt_results = r.results
+        
+        opt_solution = opt_results['solution']
+        
+        opt_solution_string = list(opt_solution.keys())[0]
+        
+        assert opt_solution_string == '11111000'
         
     def test_exact_solution(self):
         """
@@ -142,48 +178,6 @@ class TestingQAOABackendAnalyticalSimulator(unittest.TestCase):
 
         assert correct_config in config_vec
         
-
-        
-'''    
-def run_Rqaoa_experiment():  
-    
-    g = random_k_regular_graph(degree=3, nodes=range(8), weighted=True, biases=False) 
-
-    # Define te problem and translate it into a binary Qubo.
-    maxcut_prob = MaximumCut(g)
-    maxcut_qubo = maxcut_prob.get_qubo_problem()
-
-    
-    # Define the RQAOA object (default rqaoa_type = 'adaptive')
-    r = RQAOA()
-
-    # Set parameters for RQAOA, in this case we fix the n_max to 1 (default), the final cutoff value to 3
-    r.set_rqaoa_parameters(steps=1, n_cutoff=3)
-
-    ## Setting up the QAOA properties
-    init_beta = 0.42
-    init_gamma = 0.42
-
-    r.set_circuit_properties(p=1, init_type='custom', variational_params_dict={"betas":[init_beta], "gammas":[init_gamma]}, mixer_hamiltonian='x')
-
-    # Define the device you want to run your problem on using the create_device() function - Here we choose the local wavefunction simulator supported by OpenQAOA
-    device = create_device(location='local', name='analytical_simulator')
-    r.set_device(device)
-    
-    # Set the classical method used to optimiza over QAOA angles and its properties
-    r.set_classical_optimizer(method="COBYLA", optimizer_options = {"stepsize" : 10**(-10)}, tol=10**(-1), maxfev=1, maxiter=1, optimization_progress=False, cost_progress=False, parameter_log=False)
-
-   
-    # Compile problem instance on RQAOA, just like with QAOA
-    r.compile(maxcut_qubo)
-    
-    r.optimize()
-    
-    print(r.results)
-    
-    
-run_Rqaoa_experiment()    
-'''
 
 if __name__ == "__main__":
     unittest.main()
