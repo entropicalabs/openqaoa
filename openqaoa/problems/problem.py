1c2ca82d0a5cd10d0441037cbede0b46bf8de4c2
Raul Conchello Vendrell <raul@entropicalabs.com>
2022-12-29 10:49:34 +0000

    node_link_graph comment

--- openqaoa/problems/problem.py
+++ openqaoa/problems/problem.py
@@ -49,7 +49,7 @@ class Problem(ABC):
         for key, value in self.__dict__.items():
             # remove "_" from the beginning of the key if it exists
             new_key = key[1:] if key.startswith("_") else key
-            # convert networkx graphs to dictionaries
+            # convert networkx graphs to dictionaries for serialization (to get back to a graph, use nx.node_link_graph)
             new_value = nx.node_link_data(value) if isinstance(value, nx.Graph) else value
             yield (new_key, new_value)
 
