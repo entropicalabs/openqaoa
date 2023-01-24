ff666ce8c5618cc1d60d0aca179c84904b07abeb
Shahidee <shahidee44@hotmail.com>
2023-01-24 15:04:21 +0800

    Added openqaoa-qiskit

---
+++ openqaoa/openqaoa-qiskit/backends/__init__.py
@@ -0,0 +1,3 @@
+from .devices import DeviceQiskit
+from .qaoa_qiskit_qpu import QAOAQiskitQPUBackend
+from .qaoa_qiskit_sim import (QAOAQiskitBackendShotBasedSimulator, QAOAQiskitBackendStatevecSimulator)
