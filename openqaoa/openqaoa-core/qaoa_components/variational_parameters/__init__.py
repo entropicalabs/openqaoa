10f7d324b5f253de739ef581c30adfd4ac7ec316
Shahidee <shahidee44@hotmail.com>
2023-01-25 17:53:02 +0800

    Updated notebook 4

--- openqaoa/openqaoa-core/qaoa_components/variational_parameters/__init__.py
+++ openqaoa/openqaoa-core/qaoa_components/variational_parameters/__init__.py
@@ -1,6 +1,6 @@
 from .variational_baseparams import QAOAVariationalBaseParams
 from .variational_params_converters import converter
-from .variational_params_factory import create_qaoa_variational_params
+from .variational_params_factory import (create_qaoa_variational_params, qaoa_variational_params_converter)
 
 from .annealingparams import QAOAVariationalAnnealingParams
 from .extendedparams import QAOAVariationalExtendedParams
