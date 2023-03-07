2b3476a146e63e6135f1da731e835a8f7442dd12
shahidee44@hotmail.com <shahidee44@hotmail.com>
2023-03-07 02:40:31 +0000

    Updated error message

--- src/openqaoa-qiskit/backends/gates_qiskit.py
+++ src/openqaoa-qiskit/backends/gates_qiskit.py
@@ -109,4 +109,4 @@ class QiskitGateApplicator(gates_core.GateApplicator):
                 # *args must be of the following format -- (qubit_1,qubit_2,circuit)
                 return self.apply_2q_fixed_gate(selected_qiskit_gate, *args)
         else:
-            raise ValueError("Error applying the requested gate. Please check in the input")
+            raise ValueError("Only 1 and 2-qubit gates are supported.")
