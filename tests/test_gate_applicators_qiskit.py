d0759175449d64dcc42b55ce852c18f204ee08b4
shahidee44@hotmail.com <shahidee44@hotmail.com>
2023-03-07 02:39:02 +0000

    Added Qiskit GateApplicator unittests, Renamed unittest file

--- tests/test_gate_applicators_qiskit.py
+++ tests/test_gate_applicators_qiskit.py
@@ -1,31 +1,33 @@
 import unittest
 
-from braket.circuits import gates as brk_gate
-from braket.circuits import Circuit
+from qiskit.circuit import gate as qsk_gate
+from qiskit.circuit import controlledgate as qsk_c_gate
+from qiskit.circuit.library import standard_gates as qsk_s_gate
+from qiskit import QuantumCircuit
 
 import openqaoa
 import openqaoa.qaoa_components.ansatz_constructor.gates as oq_gate_mod
-from openqaoa_braket.backends.gates_braket import BraketGateApplicator
+from openqaoa_qiskit.backends.gates_qiskit import QiskitGateApplicator
 from openqaoa.qaoa_components.ansatz_constructor.rotationangle import RotationAngle
 
-class TestBraketGateApplicator(unittest.TestCase):
+class TestQiskitGateApplicator(unittest.TestCase):
     
     def setUp(self):
         
-        available_gates = [each_subclass_gate for each_subclass_gate in brk_gate.Gate.__subclasses__()]
-        available_gates.extend([each_subclass_gate for each_subclass_gate in brk_gate.AngledGate.__subclasses__()])
+        available_gates = [each_subclass_gate for each_subclass_gate in qsk_gate.Gate.__subclasses__()]
+        available_gates.extend([each_subclass_gate for each_subclass_gate in qsk_c_gate.ControlledGate.__subclasses__()])
         
-        available_braket_gates_name = [each_gate.__name__.lower() for each_gate in available_gates]
-        self.available_braket_gates = dict(zip(available_braket_gates_name, 
+        available_qiskit_gates_name = [each_gate.__name__.lower() for each_gate in available_gates]
+        self.available_qiskit_gates = dict(zip(available_qiskit_gates_name, 
                                                available_gates))
         
         self.oq_available_gates = tuple([each_gate for each_gate in oq_gate_mod.OneQubitGate.__subclasses__()]) + tuple([each_gate for each_gate in oq_gate_mod.OneQubitRotationGate.__subclasses__()]) + tuple([each_gate for each_gate in oq_gate_mod.TwoQubitGate.__subclasses__()]) + tuple([each_gate for each_gate in oq_gate_mod.TwoQubitRotationGate.__subclasses__()])
         
         # OneQubitGate, OneQubitRotationGate, TwoQubitGate, TwoQubitRotationGate are not acceptable inputs into the function even though they are of the Gate class
-        # RXY, RZX and RYZ are not supported by Braket library
-        self.braket_excluded_gates = [oq_gate_mod.OneQubitGate, oq_gate_mod.OneQubitRotationGate, 
+        # RXY, RiSWAP and RYZ are not supported by Qiskit library
+        self.qiskit_excluded_gates = [oq_gate_mod.OneQubitGate, oq_gate_mod.OneQubitRotationGate, 
                           oq_gate_mod.TwoQubitGate, oq_gate_mod.TwoQubitRotationGate, 
-                          oq_gate_mod.RXY, oq_gate_mod.RZX, oq_gate_mod.RYZ]
+                          oq_gate_mod.RXY, oq_gate_mod.RiSWAP, oq_gate_mod.RYZ]
     
     def test_gate_applicator_mapper(self):
         
@@ -34,9 +36,9 @@ class TestBraketGateApplicator(unittest.TestCase):
         are trivially support by the library.
         """
 
-        for each_gate in BraketGateApplicator.BRAKET_OQ_GATE_MAPPER.values():
-            
-            self.assertTrue(each_gate.__name__ in self.available_braket_gates.keys(), '{}, {}'.format(each_gate.__name__, self.available_braket_gates.keys()))
+        for each_gate in QiskitGateApplicator.QISKIT_OQ_GATE_MAPPER.values():
+
+            self.assertTrue(each_gate.__name__.lower() in self.available_qiskit_gates.keys(), '{}, {}'.format(each_gate.__name__, self.available_qiskit_gates.keys()))
             
     def test_gate_selector(self):
         
@@ -45,63 +47,55 @@ class TestBraketGateApplicator(unittest.TestCase):
         Gate object.
         """
         
-        gate_applicator = BraketGateApplicator()
+        gate_applicator = QiskitGateApplicator()
         
         oq_gate_list = tuple([each_gate for each_gate in oq_gate_mod.Gate.__subclasses__()])
         
         oq_gate_list_1q = oq_gate_list + tuple([each_gate for each_gate in oq_gate_mod.OneQubitGate.__subclasses__()])
         
         for each_gate in oq_gate_list_1q:
-            if each_gate not in self.braket_excluded_gates:
+            if each_gate not in self.qiskit_excluded_gates:
                 
                 returned_gate = gate_applicator.gate_selector(each_gate(applicator=None, qubit_1=None))
                 
                 self.assertEqual(
-                    getattr(
-                        self.available_braket_gates[returned_gate.__name__], 
-                        returned_gate.__name__), 
+                    self.available_qiskit_gates[returned_gate.__name__.lower()],
                     returned_gate
                 )
         
         oq_gate_list_1qr = oq_gate_list + tuple([each_gate for each_gate in oq_gate_mod.OneQubitRotationGate.__subclasses__()])
         
         for each_gate in oq_gate_list_1qr:
-            if each_gate not in self.braket_excluded_gates:
+            if each_gate not in self.qiskit_excluded_gates:
                 
                 returned_gate = gate_applicator.gate_selector(each_gate(applicator=None, qubit_1=None, rotation_object=None))
-                
+
                 self.assertEqual(
-                    getattr(
-                        self.available_braket_gates[returned_gate.__name__], 
-                        returned_gate.__name__), 
+                    self.available_qiskit_gates[returned_gate.__name__.lower()],
                     returned_gate
                 )
         
         oq_gate_list_2q = oq_gate_list + tuple([each_gate for each_gate in oq_gate_mod.TwoQubitGate.__subclasses__()])
         
         for each_gate in oq_gate_list_2q:
-            if each_gate not in self.braket_excluded_gates:
+            if each_gate not in self.qiskit_excluded_gates:
                 
                 returned_gate = gate_applicator.gate_selector(each_gate(applicator=None, qubit_1=None, qubit_2=None))
                 
                 self.assertEqual(
-                    getattr(
-                        self.available_braket_gates[returned_gate.__name__], 
-                        returned_gate.__name__), 
+                    self.available_qiskit_gates[returned_gate.__name__.lower()],
                     returned_gate
                 )
         
         oq_gate_list_2qr = oq_gate_list + tuple([each_gate for each_gate in oq_gate_mod.TwoQubitRotationGate.__subclasses__()])
         
         for each_gate in oq_gate_list_2qr:
-            if each_gate not in self.braket_excluded_gates:
+            if each_gate not in self.qiskit_excluded_gates:
                 
                 returned_gate = gate_applicator.gate_selector(each_gate(applicator=None, qubit_1=None, qubit_2=None, rotation_object=None))
                 
                 self.assertEqual(
-                    getattr(
-                        self.available_braket_gates[returned_gate.__name__], 
-                        returned_gate.__name__), 
+                    self.available_qiskit_gates[returned_gate.__name__.lower()],
                     returned_gate
                 )
                 
@@ -113,13 +107,13 @@ class TestBraketGateApplicator(unittest.TestCase):
         The unsupported Gate object does not exist on the mapper.
         """
         
-        unsupported_list = [oq_gate_mod.RXY, oq_gate_mod.RZX, oq_gate_mod.RYZ]
+        unsupported_list = [oq_gate_mod.RXY, oq_gate_mod.RiSWAP, oq_gate_mod.RYZ]
         qubit_1 = 0
         qubit_2 = 1
         rotation_object = None
-        circuit = Circuit()
+        circuit = QuantumCircuit(2)
         
-        gate_applicator = BraketGateApplicator()
+        gate_applicator = QiskitGateApplicator()
         
         for each_gate in unsupported_list:
             with self.assertRaises(KeyError):
@@ -146,15 +140,15 @@ class TestBraketGateApplicator(unittest.TestCase):
                         'n_qubits': 2
                        }
                       }
-        circuit = Circuit()
-        gate_applicator = BraketGateApplicator()
+        circuit = QuantumCircuit(2)
+        gate_applicator = QiskitGateApplicator()
         
         for each_gate_set, input_arguments in input_gates.items():
             for each_gate in each_gate_set:
                 each_gate.n_qubits = input_arguments['n_qubits']
-                if each_gate not in self.braket_excluded_gates:
+                if each_gate not in self.qiskit_excluded_gates:
                     with self.assertRaises(TypeError):
-                        gate_applicator.apply_gate(each_gate, *input_arguments, circuit)
+                        gate_applicator.apply_gate(each_gate, *input_arguments['wrong_args'], circuit)
                         
     def test_wrong_n_qubits(self):
         
@@ -164,19 +158,19 @@ class TestBraketGateApplicator(unittest.TestCase):
         """
         
         input_gates = {tuple(oq_gate_mod.OneQubitRotationGate.__subclasses__()): 
-                       {'wrong_args': [0, 1, None], 
+                       {'args': [0, None], 
                         'n_qubits': 3
                        }
                       }
-        circuit = Circuit()
-        gate_applicator = BraketGateApplicator()
+        circuit = QuantumCircuit(2)
+        gate_applicator = QiskitGateApplicator()
         
         for each_gate_set, input_arguments in input_gates.items():
             for each_gate in each_gate_set:
                 each_gate.n_qubits = input_arguments['n_qubits']
-                if each_gate not in self.braket_excluded_gates:
+                if each_gate not in self.qiskit_excluded_gates:
                     with self.assertRaises(ValueError):
-                        gate_applicator.apply_gate(each_gate, *input_arguments, circuit)
+                        gate_applicator.apply_gate(each_gate, *input_arguments['args'], circuit)
                     break
                     
     def test_static_methods_1q(self):
@@ -188,14 +182,12 @@ class TestBraketGateApplicator(unittest.TestCase):
         that call this method through apply_gate.
         """
         
-        circuit = Circuit()
-        gate_applicator = BraketGateApplicator()
-        
-        circuit = Circuit()
-        output_circuit = gate_applicator.apply_1q_fixed_gate(brk_gate.X.x, 0, circuit)
+        gate_applicator = QiskitGateApplicator()
+        circuit = QuantumCircuit(1)
+        output_circuit = gate_applicator.apply_1q_fixed_gate(qsk_s_gate.XGate, 0, circuit)
 
-        self.assertEqual([qubit for qubit in output_circuit.instructions[0].target], [0])
-        self.assertEqual(output_circuit.instructions[0].operator.name.lower(), 'x')
+        self.assertEqual([each_qubit_ci.index for circuit_instruction in output_circuit.data for each_qubit_ci in circuit_instruction.qubits], [0])
+        self.assertEqual([circuit_instruction.operation.name for circuit_instruction in output_circuit.data], ['x'])
                     
     def test_static_methods_1qr(self):
         
@@ -204,20 +196,20 @@ class TestBraketGateApplicator(unittest.TestCase):
         gate to the circuit object.
         """
 
-        gate_applicator = BraketGateApplicator()
+        gate_applicator = QiskitGateApplicator()
         input_angle = 1
         rot_obj = RotationAngle(lambda x: x, None, input_angle)
         
         each_sub_gate = [each_gate for each_gate in oq_gate_mod.OneQubitRotationGate.__subclasses__()]
         
         for each_gate in each_sub_gate:
-            circuit = Circuit()
+            circuit = QuantumCircuit(1)
             
             output_circuit = gate_applicator.apply_gate(each_gate(gate_applicator, 0, rot_obj), 0, rot_obj, circuit)
 
-            self.assertEqual(output_circuit.instructions[0].operator.angle, input_angle)
-            self.assertEqual([qubit for qubit in output_circuit.instructions[0].target], [0])
-            self.assertEqual(output_circuit.instructions[0].operator.name.lower(), gate_applicator.BRAKET_OQ_GATE_MAPPER[each_gate.__name__].__name__)
+            self.assertEqual([circuit_instruction.operation.params for circuit_instruction in output_circuit.data], [[input_angle]])
+            self.assertEqual([each_qubit_ci.index for circuit_instruction in output_circuit.data for each_qubit_ci in circuit_instruction.qubits], [0])
+            self.assertEqual([circuit_instruction.operation.name for circuit_instruction in output_circuit.data], [gate_applicator.QISKIT_OQ_GATE_MAPPER[each_gate.__name__].__name__.lower()[:-4]])
         
     def test_static_methods_2q(self):
         
@@ -226,18 +218,17 @@ class TestBraketGateApplicator(unittest.TestCase):
         gate to the circuit object.
         """
         
-        circuit = Circuit()
-        gate_applicator = BraketGateApplicator()
+        gate_applicator = QiskitGateApplicator()
         
         each_sub_gate = [each_gate for each_gate in oq_gate_mod.TwoQubitGate.__subclasses__()]
         
         for each_gate in each_sub_gate:
-            if each_gate not in self.braket_excluded_gates:
-                circuit = Circuit()
+            if each_gate not in self.qiskit_excluded_gates:
+                circuit = QuantumCircuit(2)
                 output_circuit = gate_applicator.apply_gate(each_gate(gate_applicator, 0, 1), 0, 1, circuit)
 
-                self.assertEqual([qubit for qubit in output_circuit.instructions[0].target], [0, 1])
-                self.assertEqual(output_circuit.instructions[0].operator.name.lower(), gate_applicator.BRAKET_OQ_GATE_MAPPER[each_gate.__name__].__name__)
+                self.assertEqual([each_qubit_ci.index for circuit_instruction in output_circuit.data for each_qubit_ci in circuit_instruction.qubits], [0, 1])
+                self.assertEqual([circuit_instruction.operation.name for circuit_instruction in output_circuit.data], [gate_applicator.QISKIT_OQ_GATE_MAPPER[each_gate.__name__].__name__.lower()[:-4]])
                 
     def test_static_methods_2qr(self):
         
@@ -246,21 +237,20 @@ class TestBraketGateApplicator(unittest.TestCase):
         gate to the circuit object.
         """
         
-        circuit = Circuit()
-        gate_applicator = BraketGateApplicator()
+        gate_applicator = QiskitGateApplicator()
         input_angle = 1
         rot_obj = RotationAngle(lambda x: x, None, input_angle)
         
         each_sub_gate = [each_gate for each_gate in oq_gate_mod.TwoQubitRotationGate.__subclasses__()]
         
         for each_gate in each_sub_gate:
-            if each_gate not in self.braket_excluded_gates:
-                circuit = Circuit()
+            if each_gate not in self.qiskit_excluded_gates:
+                circuit = QuantumCircuit(2)
                 output_circuit = gate_applicator.apply_gate(each_gate(gate_applicator, 0, 1, rot_obj), 0, 1, rot_obj, circuit)
                 
-                self.assertEqual(output_circuit.instructions[0].operator.angle, input_angle)
-                self.assertEqual([qubit for qubit in output_circuit.instructions[0].target], [0, 1])
-                self.assertEqual(output_circuit.instructions[0].operator.name.lower(), gate_applicator.BRAKET_OQ_GATE_MAPPER[each_gate.__name__].__name__)
+                self.assertEqual([circuit_instruction.operation.params for circuit_instruction in output_circuit.data], [[input_angle]])
+                self.assertEqual([each_qubit_ci.index for circuit_instruction in output_circuit.data for each_qubit_ci in circuit_instruction.qubits], [0, 1])
+                self.assertEqual([circuit_instruction.operation.name for circuit_instruction in output_circuit.data], [gate_applicator.QISKIT_OQ_GATE_MAPPER[each_gate.__name__].__name__.lower()[:-4]])
         
 
 if __name__ == "__main__":
