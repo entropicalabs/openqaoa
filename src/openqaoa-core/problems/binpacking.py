59faf8553d7166e68bc0fd1da49a1feba0c92fb7
Alejandro Montanez <alejomonbar@gmail.com>
2023-03-09 10:05:15 +0100

    Adding bin packing problem with testing

---
+++ src/openqaoa-core/problems/binpacking.py
@@ -0,0 +1,242 @@
+import numpy as np
+
+from typing import Union
+from docplex.mp.model import Model
+import matplotlib.pyplot as plt
+
+
+from .problem import Problem
+from .converters import FromDocplex2IsingModel
+
+
+class BinPacking(Problem):
+    """
+    Creates an instance of the Bin Packing problem.
+    https://en.wikipedia.org/wiki/Bin_packing_problem
+    
+    Parameters
+    ----------
+    weights: List[int]
+        The weight of the items that must be placed in the bins.
+    weight_capacity: int
+        The maximum weight the bin can hold.
+    penalty: float
+        Penalty for the weight constraint.
+        
+    Returns
+    -------
+        An instance of the Bin PAcking problem.
+    """
+
+    __name__ = "bin_packing"
+    
+    def __init__(self, weights:list=[], weight_capacity:int=0, penalty:Union[float, list]=[],
+                 n_bins:int = None, simplifications=True, method:str="slack"):
+        #include_ineqs: True if including the inequalities
+        if method not in ["slack", "unbalanced"]:
+            raise ValueError(f"The method '{method}' is not a valid method. Choose between 'slack' and 'unbalanced'")
+        self.weights = weights
+        self.weight_capacity = weight_capacity
+        self.penalty = penalty
+        self.n_items = len(weights)
+        self.method = method
+        self.simplifications = simplifications
+        self.eq_constraints = {}
+        self.ineq_constraints = {}
+        if n_bins is None:
+            self.n_bins = self.n_items
+        else:
+            self.n_bins = n_bins
+        if len(weights) > 0:
+            self.solution = self.solution_dict()
+            self.cplex_model = self.docplex_model()
+            self.n_vars = self.cplex_model.number_of_binary_variables
+            
+    def solution_dict(self):
+        solution = {f"y_{i}":None for i in range(self.n_bins)}
+        for i in range(self.n_items):
+            for j in range(self.n_bins):
+                solution[f"x_{i}_{j}"] = None
+        if self.simplifications:
+            # First simplification: we know the minimum number of bins
+            self.min_bins = int(np.ceil(np.sum(self.weights) / self.weight_capacity))
+            for j in range(self.n_bins):
+                if j < self.min_bins:
+                    solution[f"y_{j}"] = 1
+            solution["x_0_0"] = 1
+            for j in range(1, self.n_bins):
+                solution[f"x_0_{j}"] = 0
+        return solution
+
+    @staticmethod
+    def random_instance(**kwargs):
+        """
+        Creates a random instance of the Bin Packing problem.
+
+        Parameters
+        ----------
+        n_items: int
+            The number of items that must be placed in the bins.
+        seed: int
+            Seed number for choosing the random weights
+        min_weight: int
+            Minimum weight to choose the weights from
+        max_weight: int
+            Maximum weight to choose the random weights from
+        simplification: bool
+            Use the simplification version of the problem
+        method: ["slack", "unbalanced"]
+            Use one of the two methods available to enconde the inequality constraints.
+            The slack method requires extra-qubits.
+
+        Returns
+        -------
+            A random instance of the bin packing problem.
+        """
+        seed = kwargs.get("seed", 1234)
+        np.random.seed(seed)
+        weight_capacity = kwargs.get("weight_capacity", 15)
+        n_items = kwargs.get("n_items", 3)
+        n_bins = n_items
+        min_weight = kwargs.get("min_weight", 1)
+        max_weight = kwargs.get("max_weight", 7)
+        if min_weight >= max_weight:
+            raise ValueError(f"min_weight: {min_weight} must be < max_weight:{max_weight}")
+        weights = list(np.random.randint(min_weight, max_weight, n_items))
+        simplification = kwargs.get("simplification", True)
+        penalty = kwargs.get("penalty", [])
+        method = kwargs.get("method", "slack")
+        return BinPacking(weights, weight_capacity, penalty, n_bins, simplification, method)
+
+    def docplex_model(self):
+        mdl = Model("bin_packing")
+        vars_ = {}
+        for var in self.solution.keys():
+            if self.solution[var] is None:
+                vars_[var] = mdl.binary_var(var)
+            else:
+                vars_[var] = self.solution[var]
+        objective = mdl.sum([vars_[y] for y in vars_.keys() if y[0] == "y"])
+        self.vars_pos = {var.name:n for n, var in enumerate(mdl.iter_binary_vars())}
+
+        mdl.minimize(objective)
+        if self.simplifications:
+            list_items = range(1, self.n_items)
+        else:
+            list_items = range(self.n_items)
+
+        for i in list_items:
+            # First set of constraints: the items must be in any bin
+            self.eq_constraints[f"eq_{i}"] = [[self.vars_pos[f"x_{i}_{j}"] for j in range(self.n_bins)], [1]]
+            mdl.add_constraint(mdl.sum(vars_[f"x_{i}_{j}"] for j in range(self.n_bins)) == 1)
+
+        for j in range(self.n_bins):
+            # Second set of constraints: weight constraints
+            mdl.add_constraint(
+                mdl.sum((self.weights[i]/self.weight_capacity) * vars_[f"x_{i}_{j}"] for i in range(self.n_items)) <=  vars_[f"y_{j}"]
+            )
+            if self.simplifications and j < self.min_bins:
+                if j == 0:
+                    self.ineq_constraints[f"ineq_{j}"] = [[self.vars_pos[f"x_{i}_{j}"] for i in list_items], [self.weight_capacity - self.weights[0]]]
+                else:
+                    self.ineq_constraints[f"ineq_{j}"] = [[self.vars_pos[f"x_{i}_{j}"] for i in list_items], [self.weight_capacity * self.vars_pos[f"y_{j}"]]]
+                    
+            else:
+                self.ineq_constraints[f"ineq_{j}"] = [[self.vars_pos[f"x_{i}_{j}"] for i in list_items], [self.weight_capacity * self.vars_pos[f"y_{j}"]]]
+
+        return mdl
+
+    @property
+    def qubo(self):
+        """
+        Returns the QUBO encoding of this problem.
+        
+        Returns
+        -------
+            The QUBO encoding of this problem.
+        """
+        if len(self.penalty) > 0:
+            if self.method == "slack":
+                qubo_docplex = FromDocplex2IsingModel(self.cplex_model, multipliers=self.penalty)
+            elif self.method == "unbalanced":
+                qubo_docplex = FromDocplex2IsingModel(self.cplex_model, multipliers=self.penalty[0], unbalanced_const=True, strength_ineq=self.penalty[1:])
+        else:
+            if self.method == "slack":
+                qubo_docplex = FromDocplex2IsingModel(self.cplex_model)
+            elif self.method == "unbalanced":
+                qubo_docplex = FromDocplex2IsingModel(self.cplex_model, unbalanced_const=True)
+            
+        return qubo_docplex.ising_model
+
+    def classical_solution(self, string:bool=False):
+        """
+        Return the classical solution of the bin packing problem
+
+        Parameters
+        ----------
+        string : bool, optional
+            If the solution is returned as a string. The default is False.
+
+        Raises
+        ------
+        ValueError
+            A flag if docplex does not find a valid solution.
+
+        Returns
+        -------
+        solution : Union[str, dict]
+            The classical solution of the specific problem as a string or a dict.
+
+        """
+        docplex_sol = self.cplex_model.solve()
+        if string:
+            solution = ""
+        else:
+            solution = self.solution.copy()
+        for var in self.cplex_model.iter_binary_vars():
+            if string:
+                solution += str(int(np.round(docplex_sol.get_value(var), 1)))
+            else:
+                solution[var.name] = int(np.round(docplex_sol.get_value(var), 1))
+        if not docplex_sol.is_valid_solution():
+            raise ValueError("The Cplex solver does not find a solution.")
+        return solution
+
+    def plot_solution(self, solution:Union[dict, str], ax=None):
+        """
+        A visualization method for the bin packing problem solution.
+
+        Parameters
+        ----------
+        solution : Union[dict, str]
+            The solution of the specific bin packing problem as a string or dictionary.
+        ax : TYPE, optional
+            DESCRIPTION. The default is None.
+
+        Returns
+        -------
+        fig : matplotlib.pyplot.Figure()
+            The plot visualization of the solution.
+
+        """
+        if isinstance(solution, str):
+            sol = self.solution.copy()
+            for n, var in enumerate(self.cplex_model.iter_binary_vars()):
+                sol[var.name] = int(solution[n])
+            solution = sol
+        colors = plt.cm.get_cmap("jet", len(self.weights))
+        if ax is None:
+            fig, ax = plt.subplots()
+        for j in range(self.n_bins):
+            sum_items = 0
+            if solution[f"y_{j}"]:
+                for i in range(self.n_items):
+                    if solution[f"x_{i}_{j}"]:
+                        ax.bar(j, self.weights[i], bottom=sum_items, label=f"item {i}", color=colors(i), alpha=0.7, edgecolor="black")
+                        sum_items += self.weights[i]
+        ax.hlines(self.weight_capacity, -0.5, self.n_bins - 0.5, linestyle="--", color="black", label="Max Weight")
+        ax.set_xticks(np.arange(self.n_bins), np.arange(self.n_bins), fontsize=14)
+        ax.set_xlabel("bin", fontsize=14)
+        ax.set_ylabel("weight", fontsize=14)
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.2 + 0.011*self.n_items), ncol=5, fancybox=True, shadow=True)
+        return fig 
--- tests/test_problems.py
+++ tests/test_problems.py
@@ -11,6 +11,7 @@ from openqaoa.problems import (
     SlackFreeKnapsack,
     MaximumCut,
     MinimumVertexCover,
+    BinPacking,
 )
 from openqaoa.utilities import convert2serialize
 from openqaoa.problems.helper_functions import create_problem_from_dict
@@ -1113,6 +1114,7 @@ class TestProblem(unittest.TestCase):
             "slack_free_knapsack": SlackFreeKnapsack,
             "minimum_vertex_cover": MinimumVertexCover,
             "shortest_path": ShortestPath,
+            "bin_packing": BinPacking,
         }
 
         problems, qubos = self.__generate_random_problems()
@@ -1168,6 +1170,203 @@ class TestProblem(unittest.TestCase):
                         ), "QUBO from dict method is not correct for problem type {}".format(
                             qubo.problem_instance["problem_type"]
                         )
+
+    # TESTING BINPACKING CLASS
+    def test_binpacking_terms_weights_constant(self):
+        """Test that BinPacking creates a correct QUBO from the provided weights and terms"""
+
+        weights = [3, 4]
+        weight_capacity = 6
+        
+        terms = [[2, 3],
+                 [4, 5],
+                 [0, 2],
+                 [0, 4],
+                 [0, 6],
+                 [2, 4],
+                 [2, 6],
+                 [4, 6],
+                 [1, 3],
+                 [1, 5],
+                 [1, 7],
+                 [3, 5],
+                 [3, 7],
+                 [5, 7],
+                 [0],
+                 [1],
+                 [2],
+                 [3],
+                 [4],
+                 [5],
+                 [6],
+                 [7]]
+        weights = [1.5,
+         1.5,
+         -0.75,
+         -1.0,
+         -1.5,
+         0.5,
+         0.75,
+         1.0,
+         -0.75,
+         -1.0,
+         -1.5,
+         0.5,
+         0.75,
+         1.0,
+         1.25,
+         1.25,
+         -0.875,
+         -0.875,
+         -1.1666666666666665,
+         -1.1666666666666665,
+         -1.75,
+         -1.75]
+        constant = 10.083333333333332
+        binpacking_prob_qubo = BinPacking(weights, weight_capacity, simplifications=False).qubo
+
+        self.assertTrue(terms_list_equality(terms, binpacking_prob_qubo.terms))
+        self.assertEqual(weights, binpacking_prob_qubo.weights)
+        self.assertEqual(constant, binpacking_prob_qubo.constant)
+
+    def test_binpacking_terms_weights_constant_simplified(self):
+        """Test that BinPacking creates a correct QUBO from the provided weights and terms"""
+
+        weights = [3, 4, 5]
+        weight_capacity = 10
+        
+        terms = [[1, 2],
+                 [1, 3],
+                 [2, 3],
+                 [4, 5],
+                 [4, 6],
+                 [5, 6],
+                 [1, 4],
+                 [1, 7],
+                 [4, 7],
+                 [2, 5],
+                 [8, 2],
+                 [8, 5],
+                 [0, 3],
+                 [0, 6],
+                 [0, 9],
+                 [3, 6],
+                 [9, 3],
+                 [9, 6],
+                 [0],
+                 [1],
+                 [2],
+                 [3],
+                 [4],
+                 [5],
+                 [6],
+                 [7],
+                 [8],
+                 [9]]
+
+        weights = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.2, 0.28, 0.35, 0.2, 0.4, 0.5, -0.4,
+                    -0.5, -1.0, 0.2, 0.4, 0.5, 0.4, -1.08, -0.96, -1.36, -1.1, -0.95,
+                    -1.45, -0.14, 0.1, -0.9]
+        constant = 9.29
+        binpacking_prob_qubo = BinPacking(weights, weight_capacity).qubo
+
+        self.assertTrue(terms_list_equality(terms, binpacking_prob_qubo.terms))
+        self.assertEqual(weights, binpacking_prob_qubo.weights)
+        self.assertEqual(constant, binpacking_prob_qubo.constant)
+        
+    def test_binpacking_terms_weights_constant_unbalanced(self):
+            """Test that BinPacking creates a correct QUBO from the provided weights and terms
+            using the unbalanced penalization encoding"""
+
+            weights = [3, 4, 5]
+            weight_capacity = 10
+            
+            terms = [[1, 2],
+                     [1, 3],
+                     [2, 3],
+                     [4, 5],
+                     [4, 6],
+                     [5, 6],
+                     [1, 4],
+                     [2, 5],
+                     [0, 3],
+                     [0, 6],
+                     [3, 6],
+                     [0],
+                     [1],
+                     [2],
+                     [3],
+                     [4],
+                     [5],
+                     [6]]
+
+            weights = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.05, 0.05, -0.1, -0.125,
+                       0.05, -0.475, -0.97, -0.91, -1.01, -0.9625, -0.8875, -1.0125]
+            constant = 6.8775
+            binpacking_prob_qubo = BinPacking(weights, weight_capacity).qubo
+
+            self.assertTrue(terms_list_equality(terms, binpacking_prob_qubo.terms))
+            self.assertEqual(weights, binpacking_prob_qubo.weights)
+            self.assertEqual(constant, binpacking_prob_qubo.constant)
+
+    def test_binpacking_random_problem(self):
+        """Test Bin Packing random instance method"""
+
+        seed = 1234
+        np.random.seed(seed)
+        min_weight = 1
+        max_weight = 7
+        n_items = 3
+        max_weight = 15
+        weights = list(np.random.randint(min_weight, max_weight, n_items))
+        binpacking_manual_prob = BinPacking(weights, max_weight).qubo
+
+        binpacking_random_prob = BinPacking.random_instance(
+            n_items=3, seed=seed
+        ).qubo
+
+        self.assertTrue(
+            terms_list_equality(binpacking_manual_prob.terms, binpacking_random_prob.terms)
+        )
+        self.assertEqual(binpacking_manual_prob.weights, binpacking_random_prob.weights)
+        self.assertEqual(binpacking_manual_prob.constant, binpacking_random_prob.constant)
+
+    def test_binpacking_plot(self):
+        """Test Bin Packing random instance method"""
+        from matplotlib.pyplot import Figure
+        seed = 1234
+        binpacking_random_prob = BinPacking.random_instance(
+            n_items=3, seed=seed
+        )
+        sol = {'y_0': 1,
+         'y_1': 0,
+         'y_2': 0,
+         'x_0_0': 1,
+         'x_0_1': 0,
+         'x_0_2': 0,
+         'x_1_0': 1,
+         'x_1_1': 0,
+         'x_1_2': 0,
+         'x_2_0': 1,
+         'x_2_1': 0,
+         'x_2_2': 0}
+        fig = binpacking_random_prob.plot_solution(sol)
+        self.assertTrue(
+            isinstance(fig, Figure)
+        )
+
+    def test_binpacking_method_checking(self):
+        """
+        Checks if the type-checking returns the right error.
+        """
+        weights = [3, 5, 7]
+        weight_capacity = 15
+        method = "random"
+        with self.assertRaises(ValueError) as e:
+            BinPacking(weights, weight_capacity, method=method)
+        self.assertEqual(
+            f"The method '{method}' is not a valid method. Choose between 'slack' and 'unbalanced'", str(e.exception)
+        )
 
 
 if __name__ == "__main__":
