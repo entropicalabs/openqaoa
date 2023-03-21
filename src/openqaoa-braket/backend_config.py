cfe711d59cfb7cf2ff40f36f27dc130534f5dfce
shahidee44@hotmail.com <shahidee44@hotmail.com>
2023-03-21 09:23:00 +0000

    Changed braket location name to aws

--- src/openqaoa-braket/backend_config.py
+++ src/openqaoa-braket/backend_config.py
@@ -1,7 +1,7 @@
 from .backends import DeviceAWS, QAOAAWSQPUBackend
 
 device_access = {DeviceAWS: QAOAAWSQPUBackend}
-device_location = 'braket'
+device_location = 'aws'
 device_plugin = DeviceAWS
 device_args = {DeviceAWS: ['s3_bucket_name', 'aws_region', 'folder_name']}
 backend_args = {QAOAAWSQPUBackend: ['n_shots', 'disable_qubit_rewiring', 
