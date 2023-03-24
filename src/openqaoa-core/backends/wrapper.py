784b6d04b5f63cb376f75d29059169bf4a93d0c8
Kristina Kirova <kidiki97@gmail.com>
2023-03-24 07:54:58 +0000

    creating some calibration files for different errors, imitating the workflow but still not quite there yet, slef.calibration should not be there!

--- src/openqaoa-core/backends/wrapper.py
+++ src/openqaoa-core/backends/wrapper.py
@@ -6,135 +6,141 @@ import time
 
 from .basebackend import VQABaseBackend
 
-from ..qaoa_components.ansatz_constructor.gates import (
-    X
-)
+from ..qaoa_components.ansatz_constructor.gates import X
 
 from ..qaoa_components import Hamiltonian
 
-from ..utilities import exp_val_pair, exp_val_single, negate_counts_dictionary, calculate_calibration_factors
+from ..utilities import (
+    exp_val_pair,
+    exp_val_single,
+    negate_counts_dictionary,
+    calculate_calibration_factors,
+)
+
 
 class BaseWrapper(VQABaseBackend):
     def __init__(self, backend):
         self.backend = backend
-        
+
     def __getattr__(self, name):
         return getattr(self.backend, name)
-    
+
     def expectation(self, *args, **kwargs):
         return self.backend.expectation(*args, **kwargs)
-    
+
     def expectation_w_uncertainty(self, *args, **kwargs):
         return self.backend.expectation_w_uncertainty(*args, **kwargs)
-    
+
     def exact_solution(self, *args, **kwargs):
         return self.backend.exact_solution(*args, **kwargs)
-    
+
 
 class SPAMTwirlingWrapper(BaseWrapper):
     def __init__(self, backend, n_batches):
         super().__init__(backend)
         self.n_batches = n_batches
-    
-        filename = 20230324  # the name of where the calibration data is stored should come externally in the same way as n_batches
-        with open('calibration_data/{}.json'.format(filename), 'r') as f:
-              calibration_data = json.load(f)
-                
-        # compute here so that we do it only once
-        self.calibration_factors = calculate_calibration_factors(self.backend.qaoa_descriptor.cost_hamiltonian, calibration_data)  
-        
-        print(self.calibration_factors)
-        
-        
-    def get_counts(self, params, n_shots = None):
-        '''
+
+        self.calibration = False
+
+        if not self.calibration:
+            filename = "flip_noise"  # the name of where the calibration data is stored should come externally in the same way as n_batches
+            with open("calibration_data/{}.json".format(filename), "r") as f:
+                calibration_data = json.load(f)
+
+            # compute here so that we do it only once
+            self.calibration_factors = calculate_calibration_factors(
+                self.backend.qaoa_descriptor.cost_hamiltonian, calibration_data
+            )
+
+    def get_counts(self, params, n_shots=None):
+        """
         Modified function to...
             divide into batches
             change the self.append_state according to the schedule, s.
             get the counts and classically negate them
             combine all batches into a count dict under BFA
-        '''
-        # list of integers whose binary representation signifies which qubits or be flipped at every batch 
-        s_list = [] 
+        """
+        # list of integers whose binary representation signifies which qubits or be flipped at every batch
+        s_list = []
         for _ in range(0, self.n_batches):
-            s_list.append(random.getrandbits(self.backend.n_qubits)) 
-        #s_list = [3, 0, 1, 2] # TESTING ONLY, can be specified by the user 
-        #s_list = [1, 1, 1, 1]
-        
+            s_list.append(random.getrandbits(self.backend.n_qubits))
+        # s_list = [3, 0, 1, 2] # TESTING ONLY, can be specified by the user
+        # s_list = [1, 1, 1, 1]
+
         n_shots = self.backend.n_shots if n_shots == None else n_shots
         n_shots_batch = int(n_shots / self.n_batches)
-        
-        counts = {}
-        
-        for batch in range(0, self.n_batches):
-            #print("batch ", batch)
-            s = s_list[batch]
-            s_binary = format(s, 'b').zfill(self.backend.n_qubits) # convert to binary
-            arr = np.fromiter(s_binary, dtype=int)
-            negated_qubits = np.where(arr == 1)[0] # where the syndrome has a 1
 
-            circuit_to_append = self.backend.gate_applicator.create_quantum_circuit(self.backend.n_qubits)
-            
+        counts = {}
+
+        for batch in range(0, self.n_batches):
+            # print("batch ", batch)
+            s = s_list[batch]
+            s_binary = format(s, "b").zfill(self.backend.n_qubits)  # convert to binary
+            arr = np.fromiter(s_binary, dtype=int)
+            negated_qubits = np.where(arr == 1)[0]  # where the syndrome has a 1
+
+            circuit_to_append = self.backend.gate_applicator.create_quantum_circuit(
+                self.backend.n_qubits
+            )
+
             for negated_qubit in negated_qubits:
-                
                 negation_gate = X(self.backend.gate_applicator, negated_qubit)
-                circuit_to_append = self.backend.gate_applicator.apply_gate(negation_gate, negated_qubit, circuit_to_append)
-                #another way to do the same:
-                #circuit_to_append = self.backend.gate_applicator.apply_1q_fixed_gate(qiskit_gate=XGate, qubit_1=negated_qubit, circuit=circuit_to_append)
-                
+                circuit_to_append = self.backend.gate_applicator.apply_gate(
+                    negation_gate, negated_qubit, circuit_to_append
+                )
+                # another way to do the same:
+                # circuit_to_append = self.backend.gate_applicator.apply_1q_fixed_gate(qiskit_gate=XGate, qubit_1=negated_qubit, circuit=circuit_to_append)
+
             self.backend.append_state = circuit_to_append
-        
-            counts_batch = self.backend.get_counts(params, n_shots_batch) # should call the original get_counts of the specific backend
-            
-            negated_counts = negate_counts_dictionary(counts_dictionary = counts_batch, s = s)
-            
+
+            counts_batch = self.backend.get_counts(
+                params, n_shots_batch
+            )  # should call the original get_counts of the specific backend
+
+            negated_counts = negate_counts_dictionary(
+                counts_dictionary=counts_batch, s=s
+            )
+
             # Add to the final counts dict
             for key in negated_counts:
                 if key in counts:
                     counts[key] = counts[key] + negated_counts[key]
                 else:
                     counts.update([(key, negated_counts[key])])
-        
-        
+
         self.measurement_outcomes = counts
-        
+
         return counts
-    
-    def expectation_value_spam_twirled(self, counts: Dict, hamiltonian: Hamiltonian, calibration_factors: dict):
-        """
-        
-        """
-         
-        
+
+    def expectation_value_spam_twirled(
+        self, counts: Dict, hamiltonian: Hamiltonian, calibration_factors: dict
+    ):
+        """ """
+
         terms = [term.qubit_indices for term in hamiltonian.terms]
         coeffs = [coeff for coeff in hamiltonian.coeffs]
-        hamiltonian_as_dict = dict(zip(terms,coeffs))
-        
-        print(self.qaoa_descriptor.cost_single_qubit_coeffs[0])
-        print(hamiltonian_as_dict)
-        print(hamiltonian_as_dict[(0,)]) 
-        
+        hamiltonian_as_dict = dict(zip(terms, coeffs))
+
         energy = 0
 
         # Compute expectation values and correlations of terms present in the Hamiltonian
         for term in hamiltonian.terms:
-
             # If bias term compute expectation value
             if len(term) == 1:
                 i = term.qubit_indices[0]
                 exp_vals_z = exp_val_single(i, counts)
                 exp_vals_z /= calibration_factors[(i,)]
 
-                energy += exp_vals_z * hamiltonian_as_dict[(i,)]  
+                energy += exp_vals_z * hamiltonian_as_dict[(i,)]
 
             # If two-body term compute correlation
             elif len(term) == 2:
                 i, j = term.qubit_indices
                 exp_vals_zz = exp_val_pair((i, j), counts)
-                exp_vals_zz /= calibration_factors[(i,j)]
-                
-                energy += exp_vals_zz * hamiltonian_as_dict[(i,j)]  
-                
+                exp_vals_zz /= calibration_factors[(i, j)]
+
+                energy += exp_vals_zz * hamiltonian_as_dict[(i, j)]
+
             # If constant term, ignore
             if len(term) == 0:
                 continue
@@ -142,31 +148,29 @@ class SPAMTwirlingWrapper(BaseWrapper):
         energy += hamiltonian.constant
 
         return energy
-    
-
 
     def expectation(self, params, n_shots=None) -> float:
-        '''
+        """
         use the total counts under BFA to compute expectation values Zi and ZiZj
         correct these expectation values with the calibration data, lambda_i, lambda_ij
-        combine all corrected expectation values into the energy = cost fn to be given to the optimizer every time it calls expectation 
-        '''
+        combine all corrected expectation values into the energy = cost fn to be given to the optimizer every time it calls expectation
+        """
         counts = self.get_counts(params, n_shots)
 
-        #print("counts empty circuit ", counts)
-        
-        calibration = False
-        if calibration:
-            #timestr = time.strftime("%Y%m%d-%H%M%S") # with the time in UTC
-            timestr = time.strftime("%Y%m%d")
-            with open('calibration_data/{}.json'.format(timestr), 'w') as fp:
+        if self.calibration:
+            # filename = time.strftime("%Y%m%d-%H%M%S") # with the time in UTC
+            # filename = time.strftime("%Y%m%d")
+            # filename = 'realistic_noise'
+            # filename = 'biased_noise'
+            # filename = 'no_noise'
+            # filename = 'flip_noise'
+            with open("calibration_data/{}.json".format(filename), "w") as fp:
                 json.dump(counts, fp)
-        
-        cost = self.expectation_value_spam_twirled(counts, self.backend.qaoa_descriptor.cost_hamiltonian, self.calibration_factors)
-            
+
+        cost = self.expectation_value_spam_twirled(
+            counts,
+            self.backend.qaoa_descriptor.cost_hamiltonian,
+            self.calibration_factors,
+        )
+
         return cost
-            
-        
-    
-    
-    
