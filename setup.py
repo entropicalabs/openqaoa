6141f0f0d9ae3c55c82f5059d1ef4df7219204d7
shahidee44@hotmail.com <shahidee44@hotmail.com>
2023-05-09 03:52:24 +0000

    Updated setup.py and pyproject.toml, use setuptools for setup

--- setup.py
+++ setup.py
@@ -11,14 +11,22 @@ with open("src/openqaoa-core/openqaoa/_version.py") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
 requirements = [f"{each_folder_name}=={version}" for each_folder_name in os.listdir("src") if "openqaoa-" in each_folder_name]
+
+requirements = [
+    f'openqaoa-core @ file://localhost/{current_path}/src/openqaoa-core',
+    f'openqaoa-qiskit @ file://localhost/{current_path}/src/openqaoa-qiskit',
+    f'openqaoa-braket @ file://localhost/{current_path}/src/openqaoa-braket',
+    f'openqaoa-pyquil @ file://localhost/{current_path}/src/openqaoa-pyquil',
+    f'openqaoa-azure @ file://localhost/{current_path}/src/openqaoa-azure'
+]
     
 setup(
     name="openqaoa",
     python_requires=">=3.8, <3.11",
     version=version,
     author="Entropica Labs",
-    packages=[],
-    entry_points={"openqaoa.plugins": []}
+    packages=find_namespace_packages(where=".", exclude=["dist", "build"]),
+    entry_points={"openqaoa.plugins": []},
     url="https://github.com/entropicalabs/openqaoa",
     install_requires=requirements,
     license="MIT",
