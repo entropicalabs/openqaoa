b9897d679227cdaa91026abc87f060f46f0d7298
Ashmit JaiSarita Gupta <ashmitgupta.official@gmail.com>
2023-05-29 14:20:50 +0530

    moved test for 01_workflows_example.ipynb

--- tests/test_problems/test_maximum_cut_examples.py
+++ tests/test_problems/test_maximum_cut_examples.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import test_notebooks
+from test_notebooks import test_notebooks
 
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + "/../../")
