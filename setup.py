2ae8902f594b1d13ba5bc38e55cc909840a766a8
shahidee44@hotmail.com <shahidee44@hotmail.com>
2023-08-10 07:55:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     url="https://github.com/entropicalabs/openqaoa",
     install_requires=requirements,
     license="MIT",
-    description="OpenQAOA is a python open-source multi-backend Software Development Kit to create, customise and execute the Quantum Approximate Optimisation Algorithm (QAOA) on Noisy Intermediate-Scale Quantum (NISQ) devices, and simulators",
+    description="OpenQAOA is a python open-source multi-backend Software Development Kit to create, customise and execute the Quantum Approximate Optimisation Algorithm (QAOA) on Noisy Intermediate-Scale Quantum (NISQ) devices, and simulators.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
